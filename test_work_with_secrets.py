ac0a592a72f58e5d9fcb5f03f07e855e09310614
Иван Михеев W <ivan.a.mikheev@yandex.ru>
2023-04-27 16:10:42 +0700

    2nd tbot

---
+++ test_work_with_secrets.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+# Теперь переменная TOKEN, описанная в файле .env,
+# доступна в пространстве переменных окружения 
+
+token = os.getenv('TOKEN')
+print(token)
